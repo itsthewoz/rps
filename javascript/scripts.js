61b68a853c25b75262017c7c1a94eaa999256de8
Przemek Wozniak <Pkwznk@gmail.com>
2022-01-23 18:14:13 +0100

    added a reset function

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -18,14 +18,14 @@ const rock = document.querySelector(".rock");
 
 rock.addEventListener("click", () => {
   playerChoice = "rock";
-  playRound();
+  playGame();
 });
 
 const scissors = document.querySelector(".scissors");
 
 scissors.addEventListener("click", () => {
   playerChoice = "scissors";
-  playRound();
+  playGame();
 });
 
 function computerPlay() {
@@ -67,16 +67,24 @@ function playRound() {
     "Current Score:" + " " + playerScore + " : " + computerScore;
 }
 
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  document.querySelector(".score").textContent =
+    "Current Score:" + " " + playerScore + " : " + computerScore;
+  document.querySelector(".results").textContent = "";
+}
+
 function winner() {
   if (playerScore === 5 && computerScore < 5) {
-    win = 1;
     alert("You Win!");
+    reset();
   } else if (playerScore < 5 && computerScore === 5) {
-    win = 1;
     alert("You Lose!");
+    reset();
   } else if (playerScore === 5 && computerScore === 5) {
-    win = 1;
     alert("It's a tie!");
+    reset();
   }
 }
 
@@ -84,72 +92,3 @@ function playGame() {
   playRound();
   winner();
 }
-
-// function reset() {}
-// document.querySelector(".score").textContent =
-//   `Current Score:` + " " + playerScore + " : " + computerScore;
-
-// function playRound() {
-//   let playerHidden = 0; //arbitrary number system that is used to assign game points for playGame()
-//   let computerSelection = computerPlay();
-//   let playerSelection = window.prompt("Rock Paper Scissors!");
-//   if (
-//     (playerSelection.toLowerCase() === "paper" &&
-//       computerSelection === "rock") ||
-//     (playerSelection.toLowerCase() === "rock" &&
-//       computerSelection === "scissors") ||
-//     (playerSelection.toLowerCase() === "scissors" &&
-//       computerSelection === "paper")
-//   ) {
-//     playerHidden = 1;
-//     alert(
-//       `You win! ` +
-//         (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) +
-//         ` beats ` +
-//         (computerSelection.charAt(0).toUpperCase() +
-//           computerSelection.slice(1)) +
-//         `!`
-//     );
-//   } else if (playerSelection === computerSelection) {
-//     playerHidden = 2;
-//     alert(`It\'s a tie`);
-//   } else {
-//     playerHidden = 0;
-//     alert(
-//       `You lose ` +
-//         (computerSelection.charAt(0).toUpperCase() +
-//           computerSelection.slice(1)) +
-//         ` beats ` +
-//         playerSelection.charAt(0).toUpperCase() +
-//         playerSelection.slice(1) +
-//         `!`
-//     );
-//   }
-//   return playerHidden;
-// }
-
-// function playGame() {
-//   let playerScore = 0;
-//   let computerScore = 0;
-//   let i;
-// for (i = 0; i < 30; i++) {
-//   let hiddenPoints = playRound(); //sets a var to access playerHidden value set by playRound()
-//   if (hiddenPoints === 1) {
-//     playerScore++;
-//     if (playerScore === 5) {
-//       alert("You Win!");
-//       i = 30;
-//     }
-//   } else if (hiddenPoints === 0) {
-//     computerScore++;
-//     if (computerScore === 5) {
-//       alert("You Lose!");
-//       i = 30;
-//     }
-//   }
-
-//   alert(playerScore + " to " + computerScore);
-// }
-// }
-
-// console.log(playRound());
